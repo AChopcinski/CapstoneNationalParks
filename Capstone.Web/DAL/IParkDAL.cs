f1fe8a5757e586f995715973267c5544c76b4b9b
Anita Torrence <anita.torrence@gmail.com>
2017-03-08 10:30:49 -0500

    Park Model and DAL created

---
+++ Capstone.Web/DAL/IParkDAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Capstone.Web.Models;
+
+
+namespace Capstone.Web.DAL
+{
+    public interface IParkDAL
+    {
+        List<ParkModel> GetParks();
+        ParkModel GetParks(string ParkCode);
+    }
+}
---
+++ Capstone.Web/DAL/ParkSqlDAL.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using Capstone.Web.Models;
+using System.Data.SqlClient;
+
+
+namespace Capstone.Web.DAL
+{
+    public class ParkSqlDAL : IParkDAL
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["Capstone"].ConnectionString;
+        private const string SQL_GetParks = "SELECT * from park;";
+        public ParkSqlDAL(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public List<ParkModel> GetParks()
+        {
+            List<ParkModel> parks = new List<ParkModel>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(SQL_GetParks, conn);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        ParkModel park = new ParkModel();
+                        park.ParkCode = Convert.ToString(reader["parkCode"]);
+                        park.ParkName = Convert.ToString(reader["parkName"]);
+                        park.State = Convert.ToString(reader["state"]);
+                        park.Acreage = Convert.ToInt32(reader["acreage"]);
+                        park.ElevationInFeet = Convert.ToInt32(reader["elevationInFeet"]);
+                        park.MilesOfTrail = Convert.ToInt32(reader["milesOfTrail"]);
+                        park.NumberOfCampsites = Convert.ToInt32(reader["numberOfCampsites"]);
+                        park.Climate = Convert.ToString(reader["climate"]);
+                        park.YearFounded = Convert.ToInt32(reader["yearFounded"]);
+                        park.AnnualVisitorCount = Convert.ToInt32(reader["annualVisitorCount"]);
+                        park.InspirationalQuote = Convert.ToString(reader["inspirationalQuote"]);
+                        park.InspirationalQuoteSource = Convert.ToString(reader["inspirationalQuoteSource"]);
+                        park.ParkDescription = Convert.ToString(reader["parkDescription"]);
+                        park.EntryFee = Convert.ToInt32(reader["entryFee"]);
+                        park.NumberOfAnimalSpecies = Convert.ToInt32(reader["numberOfAnimalSpecies"]);
+                        parks.Add(park);
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                throw;
+            }
+            return parks;
+        }
+
+        public ParkModel GetParks(string ParkCode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
